d8afd0a0d3d63a6659bbed89e63c5059a9b936e4
Tharun-Kumar-A-01 <tharunkumar8906@gmail.com>
2025-08-18 16:52:30 +0530

    Some UI improvements

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const template = `<div class="event-card">
 						<a href='{{event-url}}'><button class="register-btn">Register</button></a>
 					</div>
 					<p class="location">{{event-location}}</p>
-				</div>`
+				</div>`;
 
 // template for past event cards, this one has custom classname and dont have the register button
 const past_events_template = `<div class="past event-card">
@@ -26,110 +26,145 @@ const past_events_template = `<div class="past event-card">
 						<p class="past event-time">{{event-time}}</p>
 					</div>
 					<p class="past location">{{event-location}}</p>
-				</div>`
-
+				</div>`;
 
 let events = [];
 async function run() {
+  // fetch events.json directly from github repo of tamilnadu.tech
+  const response = await fetch(
+    "https://raw.githubusercontent.com/FOSSUChennai/Communities/refs/heads/main/src/data/events.json",
+  );
+  events = await response.json();
 
-	// fetch events.json directly from github repo of tamilnadu.tech
-	const response = await fetch('https://raw.githubusercontent.com/FOSSUChennai/Communities/refs/heads/main/src/data/events.json');
-	events = await response.json();
-	
-	console.log(events);
-	
-	// this fetches past events from a different json
-	const past_events_response = await fetch('https://raw.githubusercontent.com/FOSSUChennai/Communities/refs/heads/main/src/data/pastevents.json');
-	let past_events = await past_events_response.json();
-	
-	let this_month = [];
-	let upcoming = [];
+  console.log(events);
 
-	// group each event object by date
-	events.forEach((event) => {
-		eventDate = new Date(event.eventDate);
-		today = new Date()
-		if (eventDate.getMonth() === today.getMonth() && eventDate.getDate() >= today.getDate() && eventDate.getYear() >= today.getYear()) {
-			this_month.push(event)
-		}
-		else if (eventDate.getMonth() > today.getMonth() && eventDate.getYear() >= today.getYear()) {
-			upcoming.push(event)
-		}
-		else {
-			past_events.push(event)
-		}
+  // this fetches past events from a different json
+  const past_events_response = await fetch(
+    "https://raw.githubusercontent.com/FOSSUChennai/Communities/refs/heads/main/src/data/pastevents.json",
+  );
+  let past_events = await past_events_response.json();
 
-	});
+  let this_month = [];
+  let upcoming = [];
 
-	this_month = this_month.sort((a,b)=>{
-		const A = new Date(`${a.eventDate}`);
-		const B = new Date(`${b.eventDate}`);
-		return A - B;
-	})
-	upcoming = upcoming.sort((a,b)=>{
-		const A = new Date(`${a.eventDate}`);
-		const B = new Date(`${b.eventDate}`);
-		return A - B;
-	})
-	past_events = past_events.slice(-6).sort((a,b)=>{
-		const A = new Date(`${a.eventDate}`);
-		const B = new Date(`${b.eventDate}`);
-		return B - A;
-	})
-	// debugging purpose
-	// console.log(this_month)
-	// console.log(upcoming)
-	// console.log(past_events)
+  // group each event object by date
+  events.forEach((event) => {
+    eventDate = new Date(event.eventDate);
+    today = new Date();
+    if (
+      eventDate.getMonth() === today.getMonth() &&
+      eventDate.getDate() >= today.getDate() &&
+      eventDate.getYear() >= today.getYear()
+    ) {
+      this_month.push(event);
+    } else if (
+      eventDate.getMonth() > today.getMonth() &&
+      eventDate.getYear() >= today.getYear()
+    ) {
+      upcoming.push(event);
+    } else {
+      past_events.push(event);
+    }
+  });
 
-	let rendered = '';
-	if (this_month.length == 0) {
-		rendered = "<h1>No events known this month</h1>";    // fallback message
-	} else {
-		this_month.map(({ communityName, eventName, eventVenue, eventDate, eventTime, eventLink }) => {
-			rendered += template.replace("{{community-name}}", communityName)     // replace fields of template string with date
-				.replace("{{event-title}}", eventName)
-				.replace("{{event-date}}", eventDate.split('-').reverse().join('-'))
-				.replace("{{event-time}}", eventTime)
-				.replace("{{event-location}}", eventVenue)
-				.replace("{{event-url}}", eventLink);
-		})
-	}
-	// renders this month events
-	this_month_events_section.innerHTML = rendered;
+  /* sorts and renders events for this month section */
+  this_month = this_month.sort((a, b) => {
+    const A = new Date(`${a.eventDate}`);
+    const B = new Date(`${b.eventDate}`);
+    return A - B;
+  });
 
-	// repeat two more times
-	rendered = '';
-	if (upcoming.length == 0) {
-		rendered = "<h1>No events known to be coming up</h1>";
-	} else {
-		upcoming.map(({ communityName, eventName, eventVenue, eventDate, eventTime, eventLink }) => {
-			rendered += template.replace("{{community-name}}", communityName)
-				.replace("{{event-title}}", eventName)
-				.replace("{{event-date}}", eventDate.split('-').reverse().join('-'))
-				.replace("{{event-time}}", eventTime)
-				.replace("{{event-location}}", eventVenue)
-				.replace("{{event-url}}", eventLink);
-		})
-	}
-	// renders upcoming events
-	upcoming_events_section.innerHTML = rendered;
+  let rendered = "";
+  if (this_month.length == 0) {
+    rendered = "<h1>No events known this month</h1>"; // fallback message
+  } else {
+    this_month.map(
+      ({
+        communityName,
+        eventName,
+        eventVenue,
+        eventDate,
+        eventTime,
+        eventLink,
+      }) => {
+        rendered += template
+          .replace("{{community-name}}", communityName) // replace fields of template string with data
+          .replace("{{event-title}}", eventName)
+          .replace("{{event-date}}", eventDate.split("-").reverse().join("-"))
+          .replace("{{event-time}}", eventTime)
+          .replace("{{event-location}}", eventVenue)
+          .replace("{{event-url}}", eventLink);
+      },
+    );
+  }
+  // renders this month events
+  this_month_events_section.innerHTML = rendered;
 
-	rendered = '';
-	if (past_events.length == 0) {
-		rendered = "<h1>Memory Corrupt! No history available</h1>";
-	} else {
-		past_events.map(({ communityName, eventName, eventVenue, eventDate, eventTime, eventLink }) => {
-			rendered += past_events_template.replace("{{community-name}}", communityName)
-				.replace("{{event-title}}", eventName)
-				.replace("{{event-date}}", eventDate.split('-').reverse().join('-'))
-				.replace("{{event-time}}", eventTime)
-				.replace("{{event-location}}", eventVenue)
-				// no event-url here
-		})
-	}
-	// renders past events
-	past_events_section.innerHTML = rendered;
-	
+  /* doing the same for upcoming sections */
+  upcoming = upcoming.sort((a, b) => {
+    const A = new Date(`${a.eventDate}`);
+    const B = new Date(`${b.eventDate}`);
+    return A - B;
+  });
+
+  rendered = "";
+  if (upcoming.length == 0) {
+    rendered = "<h1>No events known to be coming up</h1>";
+  } else {
+    upcoming.map(
+      ({
+        communityName,
+        eventName,
+        eventVenue,
+        eventDate,
+        eventTime,
+        eventLink,
+      }) => {
+        rendered += template
+          .replace("{{community-name}}", communityName)
+          .replace("{{event-title}}", eventName)
+          .replace("{{event-date}}", eventDate.split("-").reverse().join("-"))
+          .replace("{{event-time}}", eventTime)
+          .replace("{{event-location}}", eventVenue)
+          .replace("{{event-url}}", eventLink);
+      },
+    );
+  }
+  // renders upcoming events
+  upcoming_events_section.innerHTML = rendered;
+
+  past_events = past_events
+    .slice(window.innerWidth >= 1829 ? -8 : -6)
+    .sort((a, b) => {
+      const A = new Date(`${a.eventDate}`);
+      const B = new Date(`${b.eventDate}`);
+      return B - A;
+    });
+  rendered = "";
+  if (past_events.length == 0) {
+    rendered = "<h1>Memory Corrupt! No history available</h1>";
+  } else {
+    past_events.map(
+      ({
+        communityName,
+        eventName,
+        eventVenue,
+        eventDate,
+        eventTime,
+        eventLink,
+      }) => {
+        rendered += past_events_template
+          .replace("{{community-name}}", communityName)
+          .replace("{{event-title}}", eventName)
+          .replace("{{event-date}}", eventDate.split("-").reverse().join("-"))
+          .replace("{{event-time}}", eventTime)
+          .replace("{{event-location}}", eventVenue);
+        // no event-url here
+      },
+    );
+  }
+  // renders past events
+  past_events_section.innerHTML = rendered;
 }
 
-run();    // initiate fetching and rendering
+run(); // initiate fetching and rendering
